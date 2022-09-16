e6ef439e1b5a2a618e0b379fba5d5d778ba69789
AviansEpic <64453220+AviansEpic@users.noreply.github.com>
2022-09-15 22:19:57 -0700

    Update output.js

--- output.js
+++ output.js
@@ -1,1 +1,9 @@
-// Output
+// Deobfuscated by Avian!
+// I apologize for variables looking like shit
+
+
+function add(deobf_var_e5b3x2, deobf_var_e5b3x3) {
+    return deobf_var_e5b3x2 + deobf_var_e5b3x3
+}
+let result = add(1, 5);
+console["log"](result)

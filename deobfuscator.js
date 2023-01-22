81646242dc3d001f369199eaf07f04b4b736fc2d
AviansEpic <64453220+AviansEpic@users.noreply.github.com>
2023-01-21 22:35:12 -0700

    Update deobfuscator.js

--- deobfuscator.js
+++ deobfuscator.js
@@ -5,7 +5,7 @@
 const fs = require('fs');
 const beautify = require('js-beautify').js;
 
-let debug = false; // If you want to debug, set this to true
+let debug = true; // If you want to debug, set this to true
 
 let jsCode = fs.readFileSync("input.js",{encoding:'utf-8',flag:'r'}); // Obfuscated code
 
@@ -16,33 +16,55 @@ let strings = eval('('+stringsArray+')'); // Converts the strings into an actual
 let stringsName = jsCode.split(";")[0].split("=")[0].split("var ")[1]; // Gets the string array name
 
 
-if(debug){console.log("String array name: "+stringsName);console.log("Strings:");console.log(strings);console.log("Cleaning up...");} // Debug stuff
+if(debug) {
+    console.log("String array name: "+stringsName);
+    console.log("Strings:");console.log(strings);
+}
 
+console.log("Cleaning up...");
 
 //Beautifing the code
 jsCode = jsCode.replaceAll(stringsName,"deobfed"); // Replacing the string array with a new one, which is more readable and recognizable
-jsCode = jsCode.replaceAll("_0x","deobf_var_") // Replacing all annoying variables, with a new one (this is still annoying, but if you can fix it, please try to contribute this repo)
+jsCode = jsCode.replaceAll("_0x","var_") // Replacing all annoying variables
 jsCode = beautify(jsCode)
 
-let vars = /deobf_var_(\w+)/.exec(jsCode) // yeah regex
+let vars = jsCode.matchAll(/var_(\w+)/g) // yeah regex
 
+// making vars cool (example: var_1)
 if (vars != null) {
-    for (let i = 1; i < vars.length; i++) {
-        jsCode.replaceAll(vars[i], i.toString());
+    let count = 1;
+    for (const match of vars) {
+        jsCode = jsCode.replaceAll(match[1], count);
+	    count++
     }
 }
+
+let goodChars = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+    's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+    'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+
 
 for(let i=0;i<strings.length;i++) {
     let string = strings[i].replaceAll('"','\\"'); // Making sure we don't have a qoutation mark in our qoutation marks.
 
     jsCode = jsCode.replaceAll("deobfed["+i+"]",'"'+string+'"'); // Replacing the strings array with a new non-annoying string.
-    
+
+    if (goodChars.includes(string[0])) {
+        jsCode = jsCode.replaceAll(`["${string}"]`, `.${string}`)
+    }
 }
+
+// little beautify
 
 jsCode = jsCode.split("\n").slice(1).join('\n'); // Removing the strings array
 
 console.log("Cleaned code!\n");
 
-fs.writeFileSync("output.js","// Deobfuscated by Avian!\n// I apologize for variables looking like shit\n\n"+jsCode); // Writing to the file and adding credits.
+fs.writeFileSync("output.js","// Deobfuscated by Avian!\n// Variables look decent now.\n\n"+jsCode); // Writing to the file and adding credits.
 
 console.log("Deobfuscated, check output.js");
